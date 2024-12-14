8687f79d6a04aa14c7671148847af865201a60d1
GIOhnny <giohnny@gmail.com>
2024-12-15 00:39:54 +0200

    C16-113

--- C16/main.py
+++ C16/main.py
@@ -7,8 +7,17 @@ from turtle import Turtle, Screen
 timmy = Turtle()
 print(timmy)
 timmy.shape("turtle")
-timmy.color("red")
+timmy.color("aquamarine")
+timmy.forward(100)
 
 my_screen = Screen()
 print(my_screen.canvheight)
 my_screen.exitonclick()
+
+from prettytable import PrettyTable
+table = PrettyTable()
+table.add_column("Pokemon Name",["Pikachu","Squitle","Charmander"])
+table.add_column("Type",["Electric","Water","Fire"])
+table.align = "l"
+
+print(table)
