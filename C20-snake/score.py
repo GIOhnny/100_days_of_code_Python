542b694d7920d6b24c36f1fa7a1cc6d3ebd62b4d
GIOhnny <giohnny@gmail.com>
2024-12-17 00:34:23 +0200

    C20 - End

--- C20-snake/score.py
+++ C20-snake/score.py
@@ -13,6 +13,10 @@ class Score(Turtle):
     def update_score(self):
         self.write(f"Score: {self.score}", align="center", font=("Courier", 20, "normal"))
 
+    def game_over(self):
+        self.goto(0,0)
+        self.write("Game over!", align="center", font=("Courier", 20, "normal"))
+
     def increase_score(self):
         self.score += 1
         self.clear()
