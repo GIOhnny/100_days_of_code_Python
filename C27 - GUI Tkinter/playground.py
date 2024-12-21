802ef2fe97f8186f218b1d55d10ee20df286bc96
GIOhnny <giohnny@gmail.com>
2024-12-22 00:30:53 +0200

    C27 - kwargs

--- C27 - GUI Tkinter/playground.py
+++ C27 - GUI Tkinter/playground.py
@@ -3,7 +3,13 @@ def add(*args):
     sum=0
     for arg in args:
         sum+=arg
-    print(sum)
 
 
-add(1, 2, 3, 4, 5)  # Output: (1, 2, 3, 4, 5)
+print(add(1, 2, 3, 4, 5))  # Output: (1, 2, 3, 4, 5)
+
+def calculate(n, **kwargs):
+    n+=kwargs["add"]
+    n*=kwargs["multiply"]
+    print(n)
+
+calculate(2, add=3, multiply=5)  # Output: 25
