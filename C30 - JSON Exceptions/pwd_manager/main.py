f9e112f87268ad05b14b0885a32133ec15717bb7
GIOhnny <giohnny@gmail.com>
2025-01-20 23:50:18 +0200

    Search dictionary, json, exceptions

--- C30 - JSON Exceptions/pwd_manager/main.py
+++ C30 - JSON Exceptions/pwd_manager/main.py
@@ -62,6 +62,21 @@ def save():
             website_entry.delete(0, END)
             password_entry.delete(0, END)
 
+# ---------------------------- SEARCH PASSWORD ------------------------- #
+def search():
+    website = website_entry.get()
+    try:
+        with open(os.path.join(script_dir, "data.json"), "r") as data_file:
+            data = json.load(data_file)
+    except FileNotFoundError:
+        messagebox.showinfo(title="Error", message="No Data File Found")
+    else:
+        if website in data:
+            email = data[website]["email"]
+            password = data[website]["password"]
+            messagebox.showinfo(title=website, message=f"Emai: {email}\nPassword: {password}")
+        else:
+            messagebox.showinfo(title="Error", message=f"No details for the {website} exists.")    
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -84,7 +99,7 @@ password_label.grid(row=3, column=0)
 
 #Entries
 website_entry = Entry(width=35)
-website_entry.grid(row=1, column=1, columnspan=2)
+website_entry.grid(row=1, column=1)
 website_entry.focus()
 email_entry = Entry(width=35)
 email_entry.grid(row=2, column=1, columnspan=2)
@@ -93,9 +108,11 @@ password_entry = Entry(width=21)
 password_entry.grid(row=3, column=1)
 
 # Buttons
-generate_password_button = Button(text="Generate Password", command=generate_password)
+generate_password_button = Button(text="Generate Password", command=generate_password, width=18)
 generate_password_button.grid(row=3, column=2)
 add_button = Button(text="Add", width=36, command=save)
 add_button.grid(row=4, column=1, columnspan=2)
+search_button = Button(text="Search", width=18, command=search)
+search_button.grid(row=1, column=2)
 
 window.mainloop()
