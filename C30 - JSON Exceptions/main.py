2dedb254b6d3e2282f77dc3da9320486b93a3546
GIOhnny <giohnny@gmail.com>
2025-01-20 22:45:46 +0200

    try except else finally

---
+++ C30 - JSON Exceptions/main.py
@@ -0,0 +1,21 @@
+#File not fount
+
+try:
+    file = open("a_file.txt")
+    a_dictionary = {"key": "value"}
+    print(a_dictionary["key"])
+except FileNotFoundError:
+    file = open("a_file.txt", "w")
+    print ("File not found")
+except KeyError as error_message:
+    print(f"The key {error_message} does not exist.")
+else:
+    content = file.read()
+    print(content)
+finally:
+    file.close()
+    print("File was closed.")
+
+#with open("a_file.txt") as file:
+#    file.read()
+
